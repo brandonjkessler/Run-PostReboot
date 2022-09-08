d3a817c95f249a9c57a731270c00d65afe269ff7
Brandon Kessler <brandon.j.kessler@outlook.com>
2022-09-08 13:53:35 -0600

    FUNC: Creates a custom Function
    Creates function 'New-CustomScheduledTask'

--- Run-PostReboot.ps1
+++ Run-PostReboot.ps1
@@ -1,14 +1,28 @@
-[cmdletbinding()]
-#-- Create scheduled task
-$TaskName = "Test"
-$Action = New-ScheduledTaskAction -Execute "msg.exe" -Argument " * Test Scheduled task"
-$Trigger = New-ScheduledTaskTrigger -AtStartup
-$Principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId 'SYSTEM' -LogonType ServiceAccount
 
-##-- Check for scheduled task with matching name
-if($null -ne (Get-ScheduledTask -TaskName $TaskName)){
-    Write-Warning -Message "Task with name $TaskName found."
-} else {
-    $task = Register-ScheduledTask -TaskName $TaskName -Action $Action -Description "A Task to run once after a reboot." -Trigger $Trigger -Principal $principal
-    $Task | Set-ScheduledTask
+Function New-CustomScheduledTask{
+    [cmdletbinding()]
+    param(
+        [string]$TaskName,
+        [string]$TaskPath,
+        [string]$Execute,
+        [string]$Argument
+    )
+
+    $Action = New-ScheduledTaskAction -Execute "$Execute" -Argument "$Argument"
+    $Trigger = New-ScheduledTaskTrigger -AtStartup
+    $Principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId 'SYSTEM' -LogonType ServiceAccount
+    ##-- Check for scheduled task with matching name
+    $currentSchedTask = Get-ScheduledTask -TaskName $TaskName
+    if($null -ne $currentSchedTask){
+        Write-Warning -Message "Task with name $TaskName found."
+    } else {
+        Write-Verbose -Message "Creating Task $TaskName."
+        $task = Register-ScheduledTask -TaskName $TaskName -Action $Action -Description "A Task to run once after a reboot." -Trigger $Trigger -Principal $principal -TaskPath $TaskPath
+        $Task | Set-ScheduledTask
+    }
 }
+
+#-- Create scheduled task
+$TaskPath = 'Test'
+$TaskName = "Test"
+New-CustomScheduledTask -TaskName $TaskName -TaskPath $TaskPath -Execute "powershell.exe" -Argument "-ExecutionPolicy ByPass -NoProfile -Command Write-Output 'Information - TEST' | Out-File 'C:\Windows\Logs\TestScheduledTask.log' -Append"
